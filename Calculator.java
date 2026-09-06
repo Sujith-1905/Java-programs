9490fa676442bebc67314f81740ba8a0346406dc
Irfana-09 <irfanamahveen8@gmail.cim>
2026-09-06 10:16:38 +0530

    Add calculator program

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Difference = " + (a - b));
+        System.out.println("Product = " + (a * b));
+        System.out.println("Division = " + (a / b));
+    }
+}
